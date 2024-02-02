dca7a993e19f07bee27231d65c170294ca5faad1
seigo <utti.nbst.s@gmail.com>
2024-02-02 14:54:57 +0000

    fix: スナックバーのバグを修正

---
+++ store/index.js
@@ -0,0 +1,40 @@
+export const state = () => ({
+  success_alert: false, // 成功のSnackbarか
+  error_alert: false, // 失敗のSnackbarか
+  success_message: '', // 成功時のSnackbarに表示するメッセージ
+  error_message: '', // 失敗時のSnackbarに表示するメッセージ
+  success_snackbar_link: '', // 成功時のSnackbarに表示するメッセージのハイパーリンク
+  error_snackbar_link: '', // 失敗時のSnackbarに表示するメッセージのハイパーリンク
+})
+
+export const mutations = {
+  // 成功時のSnackbarを表示
+  // 引数 message, link
+  showSuccessSnackbar(state, payload) {
+    state.success_alert = true
+    state.success_message = payload.message
+    state.success_snackbar_link = payload.link
+  },
+
+  // 成功時のSnackbarを閉じる
+  closeSuccessSnackbar(state) {
+    state.success_alert = false
+    state.success_message = ''
+    state.success_snackbar_link = ''
+  },
+
+  // 失敗時のSnackbarを表示
+  // 引数 message, link
+  showErrorSnackbar(state, payload) {
+    state.error_alert = true
+    state.error_message = payload.message
+    state.error_snackbar_link = payload.link
+  },
+
+  // 失敗時のSnackbarを閉じる
+  closeErrorSnackbar(state) {
+    state.error_alert = false
+    state.error_message = ''
+    state.error_snackbar_link = ''
+  },
+}
