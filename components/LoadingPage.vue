96e1de351cf4351d0e2e4159ef44f9913bc5ae36
seigo <utti.nbst.s@gmail.com>
2024-07-25 08:21:05 +0000

    fix: 画像変更

--- components/LoadingPage.vue
+++ components/LoadingPage.vue
@@ -1,126 +1,77 @@
 <template>
-  <div class="wrapper">
-    <div class="loader"></div>
-    <div class="wording">
-      <div class="letter">L</div>
-      <div class="letter">o</div>
-      <div class="letter">a</div>
-      <div class="letter">d</div>
-      <div class="letter">i</div>
-      <div class="letter">n</div>
-      <div class="letter">g</div>
-      <div class="letter circle"></div>
-      <div class="letter circle"></div>
-      <div class="letter circle"></div>
-    </div>
-  </div>
+  <v-app>
+    <v-row justify="center" align-content="center">
+      <div class="loader"></div>
+    </v-row>
+  </v-app>
 </template>
 
 <style>
-.wrapper,
-.wording {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.wrapper {
-  height: 100vh;
-  flex-direction: column;
-}
-
-.circle {
-  background-color: #000;
-  border-radius: 50%;
-  margin: 2px 2px -6px;
-  height: 5px;
-  width: 5px;
-}
-
-.wording {
-  padding-top: 15px;
-}
-
-.letter {
-  font-size: 18px;
-  animation: jump 1s ease-in-out infinite;
-}
-
-.letter:nth-of-type(2) {
-  animation-delay: 0.1s;
-}
-
-.letter:nth-of-type(3) {
-  animation-delay: 0.2s;
-}
-
-.letter:nth-of-type(4) {
-  animation-delay: 0.3s;
-}
-
-.letter:nth-of-type(5) {
-  animation-delay: 0.4s;
-}
-
-.letter:nth-of-type(6) {
-  animation-delay: 0.5s;
-}
-
-.letter:nth-of-type(7) {
-  animation-delay: 0.6s;
-}
-
-.letter:nth-of-type(8) {
-  animation-delay: 0.7s;
-}
-
-.letter:nth-of-type(9) {
-  animation-delay: 0.8s;
-}
-
 .loader {
-  width: 35px;
-  height: 80px;
+  width: 64px;
+  height: 64px;
   position: relative;
-}
-
-.loader::after {
-  content: '';
-  position: absolute;
-  inset: 0;
-  padding: 3px 5px;
-  border-bottom: 4px solid #a7c7cb;
-  background: linear-gradient(#fca43c 0 0) bottom no-repeat content-box, #dbe1e3;
-  mix-blend-mode: darken;
-  animation: sip 2.5s infinite linear;
+  background: #fff;
+  border-radius: 4px;
+  overflow: hidden;
 }
 
 .loader::before {
   content: '';
   position: absolute;
-  inset: -18px calc(50% - 2px) 8px;
-  background: #eb6b3e;
-  transform-origin: bottom;
-  transform: rotate(8deg);
-}
-@keyframes sip {
-  0% {
-    background-size: 100% 100%;
-  }
-
-  100% {
-    background-size: 100% 5%;
-  }
+  left: 0;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  transform: rotate(45deg) translate(30%, 40%);
+  background: #15c0d3;
+  box-shadow: 32px -34px 0 5px #1741fd;
+  animation: slide 2s infinite ease-in-out alternate;
 }
 
-@keyframes jump {
+.loader::after {
+  content: '';
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #1741fd;
+  transform: rotate(0deg);
+  transform-origin: 35px 145px;
+  animation: rotate 2s infinite ease-in-out;
+}
+
+@keyframes slide {
   0%,
   100% {
-    transform: translateY(0);
+    bottom: -35px;
   }
 
-  50% {
-    transform: translateY(-10px);
+  25%,
+  75% {
+    bottom: -2px;
+  }
+
+  20%,
+  80% {
+    bottom: 2px;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(-15deg);
+  }
+
+  25%,
+  75% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(25deg);
   }
 }
 </style>
