50714b43986391ce87f3175860659395622bee07
seigo <utti.nbst.s@gmail.com>
2024-08-01 09:47:29 +0000

    fix: 投票ページを追加

--- pages/votes/index.vue
+++ pages/votes/index.vue
@@ -1,23 +1,83 @@
 <template>
   <v-app>
-    <v-row justify="center">
-      <v-col cols="10">
-        <ToolsBackButton></ToolsBackButton>
-        <h1 class="info-title">投票</h1>
+    <div v-if="!now_loading">
+      <v-row justify="center">
+        <v-col cols="10">
+          <ToolsBackButton></ToolsBackButton>
+          <h1 class="info-title">投票</h1>
+          <p>
+            2つの団体まで投票できます。また投票は取り消すことができません。慎重に行なってください。
+          </p>
 
-        <v-card>
-          <v-list>
-            <v-list-item
-              v-for="group of saw_groups"
-              :key="group.id"
-              :title="group.title"
-              :subtitle="group.groupname"
-            >
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
+          <v-card>
+            <v-list v-if="saw_groups.length">
+              <v-list-item v-for="group of saw_groups" :key="group.id" two-line>
+                <v-list-item-content>
+                  <div v-if="$vuetify.breakpoint.xs">
+                    <v-row>
+                      <v-col cols="8">
+                        <v-list-item-title>{{ group.title }}</v-list-item-title>
+                        <v-list-item-subtitle>{{
+                          group.groupname
+                        }}</v-list-item-subtitle>
+                      </v-col>
+                      <v-col cols="4">
+                        <div v-if="votable[group.id]">
+                          <v-btn
+                            depressed
+                            color="primary"
+                            outlined
+                            @click="createVote(group.id)"
+                            >投票</v-btn
+                          >
+                        </div>
+                        <div v-else>
+                          <v-btn depressed color="primary" outlined disabled
+                            >不可</v-btn
+                          >
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </div>
+                  <div v-else>
+                    <v-row>
+                      <v-col cols="9">
+                        <v-list-item-title>{{ group.title }}</v-list-item-title>
+                        <v-list-item-subtitle>{{
+                          group.groupname
+                        }}</v-list-item-subtitle>
+                      </v-col>
+                      <v-col cols="3">
+                        <div v-if="votable[group.id] && vote_count < 2">
+                          <v-btn
+                            depressed
+                            color="primary"
+                            outlined
+                            @click="createVote(group.id)"
+                            >投票</v-btn
+                          >
+                        </div>
+                        <div v-else>
+                          <v-btn depressed color="primary" outlined disabled
+                            >投票不可</v-btn
+                          >
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </div>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <div v-else>
+              <p>投票可能な団体がありません</p>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else>
+      <LoadingPage />
+    </div>
   </v-app>
 </template>
 
@@ -26,27 +86,91 @@ import Vue from 'vue'
 import { Group, Ticket } from '~/types/quaint'
 
 type Data = {
+  groups: Group[]
   saw_groups: Group[]
   taken_tickets: Ticket[]
+  now_loading: boolean
+  votable: { [group_id: string]: boolean } // {"group_id":"votable"}の形で投票可能かを保存する
+  vote_count: number // ユーザーが投票した数 -> まだ投票できるかの判定に利用
 }
 
 export default Vue.extend({
+  async asyncData({ $axios, payload }): Promise<Partial<Data>> {
+    if (payload !== undefined) {
+      // nuxt generate時の操作という意味
+      return { groups: payload.groups }
+    }
+    const res = await $axios.$get('/groups')
+    return { groups: res }
+  },
+
   data(): Data {
     return {
+      groups: [],
       saw_groups: [],
       taken_tickets: [],
+      now_loading: true,
+      votable: {},
+      vote_count: 0,
     }
   },
 
   async created() {
-    this.taken_tickets = await this.$axios.$get('/users/me/tickets')
+    this.vote_count = await this.$axios.$get('/users/me/count/votes')
+    this.taken_tickets = await this.$axios.$get('/users/me/tickets/active')
 
     for (const ticket of this.taken_tickets as Ticket[]) {
-      const group = await this.$axios.$get('/groups/' + ticket.group_id)
-      if (group.enable_vote) {
-        this.saw_groups.push(group)
+      const group = this.groups.find((group) => group.id === ticket.group_id)
+      if (group) {
+        if (group.enable_vote) {
+          this.saw_groups.push(group)
+
+          // ユーザーが二回投票していないかつその団体に投票していない -> 団体に投票できる
+          if (
+            this.vote_count < 2 &&
+            (await this.$axios.$get('/users/me/votes/' + group.id)) === false
+          ) {
+            this.votable[group.id] = true
+          } else {
+            this.votable[group.id] = false
+          }
+        }
       }
     }
+
+    this.now_loading = false
+  },
+
+  methods: {
+    createVote(id: string) {
+      this.$axios
+        .$post('/votes?group_id=' + id)
+        .then(() => {
+          this.$store.commit('ShowInternetSuccessSnackbar', {
+            message:
+              this.groups.find((group) => group.id === id)?.groupname +
+              'に投票しました',
+          })
+          this.votable[id] = false
+          this.vote_count += 1
+
+          // ページを再描画する
+          this.now_loading = true
+          this.now_loading = false
+        })
+        .catch((e) => {
+          if (e.response) {
+            this.$store.commit('ShowInternetErrorSnackbar', {
+              message: e.response.data.detail,
+            })
+          } else {
+            this.$store.commit('ShowInternetErrorSnackbar', {
+              message:
+                '予期せぬエラーが発生しました。IT委員にお声がけください🙇‍♂️',
+            })
+          }
+        })
+    },
   },
 })
 </script>
