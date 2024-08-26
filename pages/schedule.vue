eddb2a674ab00c72c01390cd52b3343ce90058ec
seigo <utti.nbst.s@gmail.com>
2024-08-26 10:04:35 +0000

    fix: スケジュール追加

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,9 +1,68 @@
 <template>
   <v-app>
     <v-container>
-      <ToolsBackButton />
       <h1 id="title">スケジュール</h1>
-      <p>現在開発中</p>
+      <v-row class="mb-10" justify="center">
+        <v-col cols="12" sm="6" md="6">
+          <h2 class="mb-3">9月14日</h2>
+          <h3 style="text-align: center; font-weight: normal">
+            午前8時20分 受付開始<br />
+            午後4時00分 公開終了
+          </h3>
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <h2 class="mb-3">9月15日</h2>
+          <h3 style="text-align: center; font-weight: normal">
+            午前8時20分 受付開始<br />
+            午後3時20分 公開終了
+          </h3>
+        </v-col>
+      </v-row>
+      <v-row class="my-10" justify="center">
+        <v-col cols="12">
+          <h2>整理券の配布スケジュール</h2>
+          <p class="my-5">
+            ※ 画像中の文字が非常に細かくなっていますので、拡大してご覧ください。
+          </p>
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <h4>14日</h4>
+          <v-img
+            class="img"
+            src="/images/event_schedule_1.png"
+            alt="1日目整理券配布スケジュール"
+          />
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <h4 :class="$vuetify.breakpoint.xs ? 'mt-5' : ''">15日</h4>
+          <v-img
+            class="img"
+            src="/images/event_schedule_2.png"
+            alt="2日目整理券配布スケジュール"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="mt-10" justify="center">
+        <v-col cols="12">
+          <h2>有志団体</h2>
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <h4>14日</h4>
+          <v-img
+            class="img"
+            src="/images/schedule_yushi_1.png"
+            alt="1日目有志団体スケジュール"
+          />
+        </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <h4>15日</h4>
+          <v-img
+            class="img"
+            src="/images/schedule_yushi_2.png"
+            alt="2日目有志団体スケジュール"
+          />
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
