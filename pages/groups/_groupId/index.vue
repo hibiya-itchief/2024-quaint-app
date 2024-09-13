830153835a3b829717091757aabb79734ebb147f
Uchida <utti.nbst.s@gmail.com>
2024-09-13 16:37:48 +0000

    fix: 有志団体についての注意書きを追加

--- pages/groups/_groupId/index.vue
+++ pages/groups/_groupId/index.vue
@@ -288,6 +288,11 @@
                     >ログイン</NuxtLink
                   >してください</v-card-subtitle
                 >
+                <span style="color: orange"
+                  >クラス劇以外につきましては本サイト上での整理券配布は行っておりません。各団体の説明に従ってください。</span
+                >
+                <br />
+                <br />
                 <div v-if="suitableEvents().length !== 0">
                   <div v-for="event in suitableEvents()" :key="event.id">
                     <v-card class="ma-2 d-flex">
