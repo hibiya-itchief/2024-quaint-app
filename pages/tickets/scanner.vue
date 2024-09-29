a050f1cf0eadef203cd19950b84e4457c36f5561
Uchida <utti.nbst.s@gmail.com>
2024-09-29 00:01:07 +0000

    fix: newsの変更

--- pages/tickets/scanner.vue
+++ pages/tickets/scanner.vue
@@ -5,11 +5,11 @@
         <v-col>
           <div style="height: 220px"></div>
           <div v-if="$vuetify.breakpoint.xs" class="container">
-            <h3 id="title">第48回星陵祭は終了しました。</h3>
+            <h3 id="title">第49回星陵祭は終了しました。</h3>
             <h3 id="title">お越しいただき、本当にありがとうございました。</h3>
           </div>
           <div v-else class="container">
-            <h2 id="title">第48回星陵祭は終了しました。</h2>
+            <h2 id="title">第49回星陵祭は終了しました。</h2>
             <h2 id="title">お越しいただき、本当にありがとうございました。</h2>
           </div>
         </v-col>
